f223ed5fc37c51f96ae557f22bdf4da7da55baae
AlarconJoshua14 <joshua14@estudiantec.cr>
2023-08-04 21:58:27 -0600

    Corregí los archivos

--- Tarea 1/CodigoErrores.py
+++ Tarea 1/CodigoErrores.py
@@ -4,12 +4,12 @@ def calcular_promedio(nums):
 
     for num in nums:
         suma += num
-        promedio = suma / cantidadd  # Error 1: Variable mal escrita (cantidadd en lugar de cantidad)
+        promedio = suma / cantidad  # Error 1: Variable mal escrita (cantidadd en lugar de cantidad) (Arreglado)
 
     promedio = suma / cantidad
-    return promedio# Error 2: Falta de espacio antes del comentario
+    return promedio  # Error 2: Falta de espacio antes del comentario (Arreglado)
 
 
 numeros = [5, 10, 15, 20, 25]
 resultado = calcular_promedio(numeros)
-print("El promedio es:",resultado)  # Error 3: Falta espacio después de la coma
+print("El promedio es:", resultado)  # Error 3: Falta espacio después de la coma (Arreglado)
